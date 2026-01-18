257e42518b9d51f044e008ce95271febf3cc2f69
pratikpande63-demo <pandepratik63@gmail.com>
2026-01-18 17:12:48 +0530

    New Project

---
+++ script.js
@@ -0,0 +1,11 @@
+VANTA.NET({
+  el: "#hero",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  color: 0x00ffff,
+  backgroundColor: 0x0a0a0a,
+  points: 12,
+  maxDistance: 20,
+  spacing: 15
+});
